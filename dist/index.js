4de94abbd3ed325cc0f41b97af6243c44dfd1aa1
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-06-21 21:16:13 +0800

    changes

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 import ora from "ora";
 import fetch from "node-fetch";
 const spinner = ora("Fetching Api").start();
-fetch("https://jsonplaceholder.typicode.com/users").then(res => {
+fetch("https://jsonplaceholder.typicode.com/users").then(() => {
     spinner.succeed("Data Fetched!");
 });
+//# sourceMappingURL=index.js.map
