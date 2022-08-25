b0b8520f79ec3463909716c1af3fb8cd8fd10ca7
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-08-25 19:06:39 +0800

    Revert "ts-jest"
    This reverts commit b016f55d96620e9dfeba9ca8bbfbe128467d53c3.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export const hello = () => "World";
+console.log("hello");
--- test/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { hello } from "../src";
-
-describe("Hello world", () => {
-	it("Should return world", () => {
-		expect(hello()).toBe("World");
-	});
-});
